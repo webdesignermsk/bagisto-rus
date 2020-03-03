adeef68bb00f44d3ace29d30cad2b05eff0b1a12
THE JOKER <pavel11_06@mail.ru>
2020-03-03 16:30:56 +0800

    wishlist rus

--- packages/Webkul/Customer/src/Resources/lang/ru/app.php
+++ packages/Webkul/Customer/src/Resources/lang/ru/app.php
@@ -2,16 +2,16 @@
 
 return [
     'wishlist' => [
-        'success' => 'Item Successfully Added To Wishlist',
-        'failure' => 'Item Cannot Be Added To Wishlist',
-        'already' => 'Item Already Present In Your Wishlist',
-        'removed' => 'Item Successfully Removed From Wishlist',
-        'remove-fail' => 'Item Cannot Be Removed From Wishlist',
-        'empty' => 'You Don\'t Have Any Items In Your Wishlist',
-        'select-options' => 'Need To Select Options Before Adding To Wishlist',
-        'remove-all-success' => 'All The Items From Your Wishlist Have Been Removed',
+        'success' => 'Товар успешно добавлен в список желаний',
+        'failure' => 'Товар не может быть добавлен в список желаний',
+        'already' => 'Товар уже присутствует в списке желаний',
+        'removed' => 'Товар успешно удален из списка желаний',
+        'remove-fail' => 'Товар не может быть удален из списка желаний',
+        'empty' => 'У Вас нет предметов в списке желаний',
+        'select-options' => 'Необходимо выбрать параметры перед добавлением в список желаний',
+        'remove-all-success' => 'Все предметы из списка желаний были удалены',
     ],
     'reviews' => [
-        'empty' => 'You Have Not Reviewed Any Of Product Yet'
+        'empty' => 'Вы еще не оставили ни одного отзыва'
     ]
 ];
